afcdb91164b3632c87beda771b1c4509674a8b4d
Dan Vo <danvo.vo@gmail.com>
2022-06-01 21:50:46 -0700

    completed part 3

--- part-3/index.js
+++ part-3/index.js
@@ -9,6 +9,16 @@ const App = () => (
 				'listening to hip-hop',
 			]}
 		/>
+		<Person
+			name="Steph"
+			age={16}
+			hobbies={['shooting 3s', 'exploring the Bay', 'golfing']}
+		/>
+		<Person
+			name="Klay"
+			age={32}
+			hobbies={['playing basketball', 'playing with my dog', 'driving my boat']}
+		/>
 	</div>
 );
 
